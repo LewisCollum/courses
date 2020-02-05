99c8ff45aea4e4a53b98c9ee48f4947025c61104
LewisCollum <lewiscollum4@gmail.com>
2020-02-05 18:10:58 -0500

    Rectangle class with 'limits' and 'origin'

--- computer_graphics/lab/1/lab1.js
+++ computer_graphics/lab/1/lab1.js
@@ -1,3 +1,34 @@
-//First and Last Name 
+//Lewis Collum
 
-//Code for Lab1
+function init() {
+    const canvas = document.getElementById("gl-canvas")
+    const gl = WebGLUtils.setupWebGL(canvas)
+
+    gl.viewport(0, 0, 512, 512)
+    gl.clearColor(0.0, 0.5, 0.0, 1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    const shaderProgram = initShaders(gl, "vertex-shader", "fragment-shader")
+    
+    gl.useProgram(shaderProgram)
+
+    const rectangleLimits = {
+        x: {first: 1, second: 2},
+        y: {first: 3, second: 4}}
+
+    const rectangleOrigin = {x: -1, y:-1}
+    
+    const rectangle = new Rectangle(rectangleLimits, rectangleOrigin)
+    rectangle.draw()
+}
+
+class Rectangle {
+    constructor(limits, origin) {
+        this.limits = limits
+        this.origin = origin
+    }
+
+    draw() {
+        console.log(this.limits.x.first)
+    }
+}
